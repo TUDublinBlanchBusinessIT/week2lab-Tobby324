0ffda440de252d02a048c37f57962db21a0a639d
Tobby Akinwale <tobbybrown10@gmail.com>
2025-02-10 20:32:14 +0000

    Completed Part B

---
+++ PartB.py
@@ -0,0 +1,18 @@
+#Tobby Akinwale
+#10/02/2025
+#A Program to Print Steps 1 to 9 to the Screen
+
+
+def printSteps(a,b):
+    for i in range(a,b):
+        print("Step " + str (i))
+    
+
+print("Step 1")
+print("Step 2")
+print("Step 3")
+printSteps(4,6)
+print("Step 7")
+print("Step 8")
+print("Step 9")
+
